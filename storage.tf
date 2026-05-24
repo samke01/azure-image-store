244931dfc8809fb613d982b3f17ab002e492ba8e
samke01 <128810413+samke01@users.noreply.github.com>
2026-05-24 19:35:09 +0200

    Fix storage container account reference

--- storage.tf
+++ storage.tf
@@ -13,6 +13,6 @@ resource "azurerm_storage_account" "main" {
 
 resource "azurerm_storage_container" "images" {
   name                  = var.container_name
-  storage_account_id    = azurerm_storage_account.main.id
+  storage_account_name  = azurerm_storage_account.main.name
   container_access_type = "private"
 }
