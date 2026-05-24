30ca71cd01a9a1422b7baf9ed36c6a8adabce7de
samke01 <128810413+samke01@users.noreply.github.com>
2026-05-24 21:48:36 +0200

    Fix storage account configuration and revert provider version to 3.0.2

--- storage.tf
+++ storage.tf
@@ -1,11 +1,11 @@
 resource "azurerm_storage_account" "main" {
-  name                       = var.storage_account_name
-  resource_group_name        = azurerm_resource_group.main.name
-  location                   = azurerm_resource_group.main.location
-  account_tier               = "Standard"
-  account_replication_type   = "LRS"
-  min_tls_version            = "TLS1_2"
-  https_traffic_only_enabled = true
+  name                      = var.storage_account_name
+  resource_group_name       = azurerm_resource_group.main.name
+  location                  = azurerm_resource_group.main.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  min_tls_version           = "TLS1_2"
+  enable_https_traffic_only = true
 
   allow_nested_items_to_be_public = false
   tags                            = var.tags
