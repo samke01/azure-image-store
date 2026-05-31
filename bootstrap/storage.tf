06f1c89b999765eab662a3acd535eaf42aab2af2
samke01 <128810413+samke01@users.noreply.github.com>
2026-05-31 22:35:28 +0200

    style(bootstrap): align storage.tf with terraform fmt

--- bootstrap/storage.tf
+++ bootstrap/storage.tf
@@ -1,10 +1,10 @@
 resource "azurerm_storage_account" "tfstate" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  min_tls_version          = "TLS1_2"
+  name                      = var.storage_account_name
+  resource_group_name       = azurerm_resource_group.main.name
+  location                  = azurerm_resource_group.main.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  min_tls_version           = "TLS1_2"
   enable_https_traffic_only = true
 
   allow_nested_items_to_be_public = false
